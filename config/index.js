c80e31747130277b2a9c9568dae82b464aa11b73
Andres Parra <byandrev@gmail.com>
2024-04-28 14:57:59 -0500

    add firebase env variables

--- config/index.js
+++ config/index.js
@@ -3,16 +3,31 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const MODE = process.env.MODE || "dev";
 
+const FIREBASE = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY
+    ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+  client_x509_cert_url: process.env.CLIENT_CERT,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+};
+
 module.exports = {
   PORT,
   MODE,
+  FIREBASE,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
-  ADMIN_USERNAME: process.env.ADMIN_USERNAME,
-  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
   JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
 };

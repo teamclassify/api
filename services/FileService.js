86eac3d5e4457d535de1978627215d8dfd88b773
anderssonccg <anderssonccg@gmail.com>
2024-05-03 15:49:17 -0500

    refactor: improve code of update function in file service

--- services/FileService.js
+++ services/FileService.js
@@ -6,41 +6,49 @@ class FileService {
   constructor() {}
 
   async upload(data) {
-    let tokens = [], info = [];
-    const diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    let clases = [];
     for (let el in data) {
-      let tmp = [];
       if(data[el].length > 0){
-        tmp = data[el].map((x) =>{
-          let diakey = []
-          if(x['Alu max'] > 0){
-            for (const key in x) {
-              if (diasSemana.includes(key)) {
-                info = x[key].split("\r\n")
-                info.forEach(element => {
-                  const dat = element.split(" ")
-                  diakey.push({
-                    dia: key, 
-                    hora_inicio: parseInt(dat[0].slice(0,2)),
-                    hora_fin: (dat[0][dat[0].length-2] != '0' ? parseInt(dat[0].slice(6,8)) + 1 : parseInt(dat[0].slice(6,8))),
-                    sala: dat[1]
-                  });
-                });
-              }
-            }
-            return {
-              nombre: x['Nombre materia'],
-              cod_asignatura: x['Codigo materia'].slice(0, x['Codigo materia'].length - 1),
-              cod_docente: "0" + x['Cod prof'],
-              grupo: x['Codigo materia'][x['Codigo materia'].length - 1],
-              horario: diakey,
-            }
-          }
-      }).filter(x => x);
-      tokens = tokens.concat(tmp)
+        clases = clases.concat(this.getClases(data[el]));
       }
     }
-    return tokens;
+    return clases;
+  }
+
+  getClases(data){
+    const clases = data.map((x) =>{
+      if(x['Alu max'] > 0){
+        let dias = this.getDays(x)  
+        return {
+          nombre: x['Nombre materia'],
+          cod_asignatura: x['Codigo materia'].slice(0, x['Codigo materia'].length - 1),
+          cod_docente: "0" + x['Cod prof'],
+          grupo: x['Codigo materia'][x['Codigo materia'].length - 1],
+          horario: dias,
+        }
+      }
+    }).filter(x => x)
+    return clases
+  }
+
+  getDays(x){
+    const dias= []
+    const diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    for (const key in x) {
+      if (diasSemana.includes(key)) {
+        const info = x[key].split("\r\n")
+        info.forEach(element => {
+          const dat = element.split(" ")
+          dias.push({
+            dia: key, 
+            hora_inicio: parseInt(dat[0].slice(0,2)),
+            hora_fin: (dat[0][dat[0].length-2] != '0' ? parseInt(dat[0].slice(6,8)) + 1 : parseInt(dat[0].slice(6,8))),
+            sala: dat[1]
+          });
+        });
+      }
+    }
+    return dias
   }
 
   async update(id, data) {

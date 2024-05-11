e663577b97cceffacb9807a94312dcc021447807
Andres Parra <byandrev@gmail.com>
2024-05-11 17:31:39 -0500

    refactor: Update multer configuration to use disk storage for file uploads

--- routes/FileRouter.js
+++ routes/FileRouter.js
@@ -4,7 +4,17 @@ const fileController = require("../controllers/FileController");
 const verifyToken = require("../middlewares/verifyToken");
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, "doc.xlsx");
+  },
+});
+
+const upload = multer({ storage });
 
 router
   .post(

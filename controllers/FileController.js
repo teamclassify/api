fa1b067f15412d77688a934e277f81535381e1b5
Andres Parra <byandrev@gmail.com>
2024-05-11 09:09:42 -0500

    refactor: Update FileController to check user permissions before performing actions

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -1,18 +1,39 @@
 const FileService = require("../services/FileService");
 const excelToJson = require("../utils/excelToJson");
-const UserService = require("../services/UserService");
+const RolService = require("../services/RolService");
+const UsuarioRolService = require("../services/UsuarioRolService");
+
 const service = new FileService();
-const userService = new UserService();
+const usuarioRolService = new UsuarioRolService();
+const rolService = new RolService();
+
+const verifyIsAdmin = async (req) => {
+  const rolAdmin = await rolService.find({ nombre: "admin" });
+
+  if (rolAdmin.length === 0) {
+    return false;
+  }
+
+  const results = await usuarioRolService.find({
+    usuario_id: req.uid,
+    rol_id: rolAdmin[0].id,
+  });
+
+  return results.length > 0;
+};
 
 const uploadClases = async (req, res) => {
   try {
-    const result = await userService.findOne(req.uid);
-    if(result.rol_id === 2){
+    const isAdmin = await verifyIsAdmin(req);
+
+    if (isAdmin) {
       const excelData = excelToJson(req.file);
       const response = await service.uploadClases(excelData);
       res.json({ success: true, data: response });
-    }else{
-      res.status(401).json({ success: false, data: "Este usuario no tiene permisos" }); 
+    } else {
+      res
+        .status(401)
+        .json({ success: false, data: "Este usuario no tiene permisos" });
     }
   } catch (error) {
     res.status(500).send({ success: false, message: error.message });
@@ -21,13 +42,16 @@ const uploadClases = async (req, res) => {
 
 const uploadSalas = async (req, res) => {
   try {
-    const result = await userService.findOne(req.uid);
-    if(result.rol_id === 2){
+    const isAdmin = await verifyIsAdmin(req);
+
+    if (isAdmin) {
       const excelData = excelToJson(req.file);
       const response = await service.uploadSalas(excelData);
       res.json({ success: true, data: response });
-    }else{
-      res.status(401).json({ success: false, data: "Este usuario no tiene permisos" }); 
+    } else {
+      res
+        .status(401)
+        .json({ success: false, data: "Este usuario no tiene permisos" });
     }
   } catch (error) {
     res.status(500).send({ success: false, message: error.message });
@@ -36,6 +60,14 @@ const uploadSalas = async (req, res) => {
 
 const update = async (req, res) => {
   try {
+    const isAdmin = await verifyIsAdmin(req);
+
+    if (!isAdmin) {
+      res
+        .status(401)
+        .json({ success: false, data: "Este usuario no tiene permisos" });
+    }
+
     const response = await service.update(req.body);
     res.json({ success: true, data: response });
   } catch (error) {
@@ -45,6 +77,14 @@ const update = async (req, res) => {
 
 const _delete = async (req, res) => {
   try {
+    const isAdmin = await verifyIsAdmin(req);
+
+    if (!isAdmin) {
+      res
+        .status(401)
+        .json({ success: false, data: "Este usuario no tiene permisos" });
+    }
+
     const response = await service.delete(req.body);
     res.json({ success: true, data: response });
   } catch (error) {

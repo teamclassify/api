f1b4eac8c50f5edad6808d808657b3eeb8b9d65a
Andres Parra <byandrev@gmail.com>
2024-05-12 13:44:01 -0500

    refactor: Update UsuarioController to include user roles in the response

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,7 +1,11 @@
 const UserService = require("../services/UserService");
+const UsuarioRolService = require("../services/UsuarioRolService");
+const RolService = require("../services/RolService");
 const verifyIsAdmin = require("../utils/verifyIsAdmin");
 
 const service = new UserService();
+const usuarioRolService = new UsuarioRolService();
+const rolService = new RolService();
 
 async function get(req, res) {
   const [isAdmin] = await verifyIsAdmin(req.uid);
@@ -13,8 +17,25 @@ async function get(req, res) {
   }
 
   try {
+    const usersRoles = await usuarioRolService.find();
     const users = await service.find();
-    res.json({ success: true, data: users });
+    const roles = await rolService.find();
+
+    const usersToReturn = users.map((user) => {
+      const usuariosRoles = usersRoles.filter(
+        (ur) => ur.usuario_id === user.id
+      );
+
+      return {
+        ...user.dataValues,
+        roles: usuariosRoles.map((ur) => {
+          const rol = roles ? roles.find((r) => r.id === ur.rol_id) : null;
+          return rol?.nombre;
+        }),
+      };
+    });
+
+    res.json({ success: true, data: usersToReturn });
   } catch (error) {
     res.status(500).send({ success: false, message: error.message });
   }
@@ -31,6 +52,35 @@ async function getById(req, res) {
 
   try {
     const user = await service.findOne(req.params.id);
+
+    if (!user) {
+      res.status(404).json({ success: false, data: "Usuario no encontrado" });
+    }
+
+    const userRol = await usuarioRolService.find({ usuario_id: req.params.id });
+    const roles = await rolService.find();
+    const userRoles = userRol.map((ur) => {
+      const rol = roles ? roles.find((r) => r.id === ur.rol_id) : null;
+      return rol?.nombre;
+    });
+
+    res.json({ success: true, data: { ...user.dataValues, roles: userRoles } });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+}
+
+async function setRole(req, res) {
+  const [isAdmin] = await verifyIsAdmin(req.uid);
+
+  if (!isAdmin) {
+    res
+      .status(401)
+      .json({ success: false, data: "Este usuario no tiene permisos" });
+  }
+
+  try {
+    const user = await service.setRole(req.params.id, req.body.role);
     res.json({ success: true, data: user });
   } catch (error) {
     res.status(500).send({ success: false, message: error.message });
@@ -40,4 +90,5 @@ async function getById(req, res) {
 module.exports = {
   get,
   getById,
+  setRole,
 };

d12635eeb45692f0311a7b908a3353588201fd31
Andres Parra <byandrev@gmail.com>
2024-05-30 17:30:07 -0500

    refactor: Improve PrestamoService queries and add user information to findOne and findAllPending methods

--- services/PrestamoService.js
+++ services/PrestamoService.js
@@ -25,10 +25,24 @@ class PrestamoService {
   */
   async findOne(id) {
     const [res] = await db.query(`
-      SELECT p.id, p.razon, p.estado, p.cantidad_personas, p.hora_inicio, p.hora_fin, p.fecha, s.nombre as sala, e.nombre as edificio
+      SELECT
+        p.id,
+        p.razon,
+        p.estado,
+        p.cantidad_personas,
+        p.hora_inicio,
+        p.hora_fin,
+        p.fecha,
+        s.nombre as sala,
+        e.nombre as edificio,
+        u.nombre as usuario_nombre,
+        u.correo as usuario_correo,
+        ur.rol_id as usuario_rol
       FROM prestamo p
       INNER JOIN salas s ON s.id = p.sala_id
       INNER JOIN edificios e ON e.id = s.edificio_id
+      INNER JOIN usuario_rols ur ON ur.id = p.usuario_id
+      INNER JOIN usuario u ON u.id = ur.usuario_id
       WHERE p.id = ${id}
     `);
 
@@ -66,11 +80,28 @@ class PrestamoService {
   */
   async findAllPending() {
     const res = await db.query(`
-      SELECT p.id, p.razon, p.estado, p.cantidad_personas, p.hora_inicio, p.hora_fin, p.fecha, s.nombre as sala, e.nombre as edificio, p.recursos
+      SELECT
+        p.id,
+        p.razon,
+        p.estado,
+        p.cantidad_personas,
+        p.hora_inicio,
+        p.hora_fin,
+        p.fecha,
+        s.nombre as sala,
+        e.nombre as edificio,
+        p.recursos,
+        u.nombre as usuario_nombre,
+        u.correo as usuario_correo,
+        u.photo as usuario_photo,
+        ur.rol_id as usuario_rol
       FROM prestamo p
       INNER JOIN salas s ON s.id = p.sala_id
       INNER JOIN edificios e ON e.id = s.edificio_id
+      INNER JOIN usuario_rols ur ON ur.id = p.usuario_id
+      INNER JOIN usuario u ON u.id = ur.usuario_id
       WHERE p.estado = 'PENDIENTE'
+      ORDER BY p.id;
     `);
 
     return res.length > 0 ? res[0] : null;

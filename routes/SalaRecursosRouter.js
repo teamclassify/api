98c501780e6e55f0385c1509a3d2e07fa7c67de7
Andres Parra <byandrev@gmail.com>
2024-06-02 22:25:55 -0500

    feat: only allow to create, update and delete to admins In SalaRecursos

--- routes/SalaRecursosRouter.js
+++ routes/SalaRecursosRouter.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const salaRecursosController = require("../controllers/SalaRecursosController");
 const verifyToken = require("../middlewares/verifyToken");
+const onlyAdmins = require("../middlewares/onlyAdmins");
 
 router
   .get("/", salaRecursosController.get)
   .get("/:id", salaRecursosController.getById)
-  .post("/", verifyToken, salaRecursosController.create)
-  .put("/:id", verifyToken, salaRecursosController.update)
-  .delete("/:id", verifyToken, salaRecursosController._delete);
+  .post("/", verifyToken, onlyAdmins, salaRecursosController.create)
+  .put("/:id", verifyToken, onlyAdmins, salaRecursosController.update)
+  .delete("/:id", verifyToken, onlyAdmins, salaRecursosController._delete);
 
 module.exports = router;

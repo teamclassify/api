eb1cb310b10bf5aaf4121edcf84fe96c15bd82fc
anderssonccg <anderssonccg@gmail.com>
2024-06-09 09:49:40 -0500

    feat: add avaliable rooms query

--- routes/SalaRouter.js
+++ routes/SalaRouter.js
@@ -8,6 +8,7 @@ const onlyAdmins = require("../middlewares/onlyAdmins");
 router
   .get("/", salaController.get)
   .get("/building/:building", salaController.get)
+  .get("/disponibles", verifyToken, salaController.getSalasDisponibles)
   .get("/:id", salaController.getById)
   .post("/", verifyToken, onlyAdmins, salaController.create)
   .put("/:id", verifyToken, onlyAdmins, salaController.update)

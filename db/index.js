ccd00e1cc93d3906aee46f1c668aacec12c8534b
Andres Parra <byandrev@gmail.com>
2024-06-13 22:52:22 -0500

    refactor: new db connection config

--- db/index.js
+++ db/index.js
@@ -1,12 +1,17 @@
 const sequelize = require("sequelize");
 const mysql2 = require("mysql2");
 
-const { DB_NAME, DB_HOST, DB_PASSWORD, DB_USER } = require("../config");
+const { DB_NAME, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT} = require("../config");
 
-const db = new sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+const db = new sequelize({
+  database: DB_NAME,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASSWORD,
   dialect: "mysql",
   dialectModule: mysql2,
   host: DB_HOST,
+  ssl: false
 });
 
 db.authenticate()
